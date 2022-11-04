763627c4d58eb695943d4f9db944f2a40c8158f7
ricardo <rgranvilla@gmail.com>
2022-11-04 19:38:58 -0300

    feat(barber-ui): configurate next app, tailwindcss, prettier, tsconfig

---
+++ packages/barber-ui/src/pages/_app.tsx
@@ -0,0 +1,11 @@
+import { ThemeProvider } from "next-themes";
+import type { AppProps } from "next/app";
+import "../styles/globals.css";
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <ThemeProvider defaultTheme="dark">
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
+}
---
+++ packages/barber-ui/src/pages/index.tsx
@@ -0,0 +1,3 @@
+export default function Home() {
+  return <h1 className="px-4 py-2 ">Hello Barber UI</h1>;
+}
